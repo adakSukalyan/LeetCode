396f20337bb4694ad037ff5551f2ca15fd3fb879
adakSukalyan <adaksukalyan2304@gmail.com>
2026-02-02 11:43:17 +0530

    Time: 6 ms (33.57%) | Memory: 21.1 MB (32.53%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hash={}
+        n=0
         for i in range(len(nums)):
-            if nums[i] in hash:
-                hash[nums[i]]+=1
-            else:
-                hash[nums[i]]=1
-        for i in hash:
-            if hash[i]== 1:
-                return i
+            n=n^nums[i]
+        return n
