1c253be8fa8f3db34bbe236f88803d198c488a39
adakSukalyan <adaksukalyan2304@gmail.com>
2026-02-15 23:16:35 +0530

    Time: 2 ms (74.94%) | Memory: 19.9 MB (66.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,20 +5,15 @@ class Solution:
         """
         row=len(matrix)
         col=len(matrix[0])
+        r=[-1 for _ in range(row)]
+        c=[-1 for _ in range(col)]
         for i in range(row):
             for j in range(col):
                 if matrix[i][j]==0:
-                    self.infinity(matrix,i,j)
+                    r[i]=0
+                    c[j]=0
         for i in range(row):
             for j in range(col):
-                if matrix[i][j]==float('inf'):
+                if r[i]== 0 or c[j]==0:
                     matrix[i][j]=0
-    def infinity(self,matrix,row,col):
-        r=len(matrix)
-        c=len(matrix[0])
-        for i in range(r):
-            if matrix[i][col] != 0:
-                matrix[i][col]=float('inf')
-        for j in range(c):
-                if matrix[row][j] !=0:
-                    matrix[row][j]=float('inf')
+    
