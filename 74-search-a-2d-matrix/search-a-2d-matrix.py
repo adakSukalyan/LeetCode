a0593625af7f2bcfdf7963f60fbbf2684e99bb58
adakSukalyan <adaksukalyan2304@gmail.com>
2026-02-18 22:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (55.63%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n=len(matrix)
+        m=len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if target==matrix[i][j]:
+                    return True
+        else:
+             return False   
