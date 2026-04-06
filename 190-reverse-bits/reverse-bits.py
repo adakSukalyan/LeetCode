7b5bf781eb746a6cb393a71b98dd6b515bbf87f2
adakSukalyan <adaksukalyan2304@gmail.com>
2026-04-06 07:28:14 +0530

    Time: 53 ms (15.46%) | Memory: 19.1 MB (88.12%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,18 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res=""
-        while n>0:
-            rem=n%2
-            res+=str(rem)
-            n=n//2
-        x=res
-        if len(x) !=32 and len(x) < 32:
-            zero=32-len(x)
-            for i in range(zero):
-                x+="0"
-        deci=0
-        power=len(x)-1
-        for i in range(len(x)):
-            deci+=int(x[i])*(2**power)
-            power-=1
-        return deci
+        res = 0
+        for i in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        return res
