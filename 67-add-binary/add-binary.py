cef9d8a57ed1970b60ac0c8d4e052083d8b016c1
adakSukalyan <adaksukalyan2304@gmail.com>
2026-04-07 22:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (45.03%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i=len(a)-1
+        j=len(b)-1
+        carry=0
+        res=""
+        while i>=0 or j>=0 or carry:
+            sum=carry
+            if i>=0:
+                sum+=int(a[i])
+                i-=1
+            if j>=0:
+                sum+=int(b[j])
+                j-=1
+            res=str(sum%2)+res
+            carry=sum//2
+        return res
